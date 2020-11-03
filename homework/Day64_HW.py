7804b2b65d34c9f3d822459dbf65dc2f71996dbd
huayang3 <66266051+huayang3@users.noreply.github.com>
2020-11-03 18:35:10 +0800

    Day64_HW

---
+++ homework/Day64_HW.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 18:33:23 2020
+
+@author: hua_yang
+"""
+
+#測試誤差 (Test Loss) 最低能到多少? 0.013
+# 請回答你的神經元參數與 Test Loss 數 => 2 HIDDEN LAYERS , 7 neurons and 1 neuron
