ff94a85c718b6a309051dbbb690446c54e5c61b6
huayang3 <66266051+huayang3@users.noreply.github.com>
2020-11-03 18:58:15 +0800

    Day65_HW

---
+++ homework/Day65_HW.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 18:55:53 2020
+
+@author: hua_yang
+"""
+
+#挑戰看看測試誤差 (Test Loss) 最低能到多少? Test loss 0.109
+#請回答你的上述幾項參數與 Test Loss 數=> 
+#批次大小:3
+#學習速率:0.03
+#啟動函數:Tanh
+#正規化選項與參數:L2+0.001
