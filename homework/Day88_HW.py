1f0ac6de67613f12a5c54cfed47e0803d86307be
huayang3 <66266051+huayang3@users.noreply.github.com>
2020-12-21 11:24:29 +0800

    Day88_HW

--- homework/Day88_HW.py
+++ homework/Day88_HW.py
@@ -74,11 +74,67 @@ from keras.callbacks import Callback
 
 # Record_fp_tp
 class Record_tp_tn(Callback):
-    """Code Here
-    撰寫一個紀錄 ture positive 與 true negative 數量的 callbacks    
-    """
+    def on_train_begin(self, epoch, logs = {}):
+        logs = logs or {}
+        record_items = ["val_tp", "val_tn"]
+        for i in record_items:
+            if i not in self.params['metrics']:
+                self.params['metrics'].append(i)
+    
+    def on_epoch_end(self, epoch, logs = {}, thres=0.5):
+        logs = logs or {}
+        y_true = self.validation_data[1].argmax(axis = 1)
+        y_pred = self.model.predict(self.validation_data[0])
+        y_pred = (y_pred[:, 1] >= thres) * 1
+        
+        val_tp = sum(y_true*y_pred)
+        val_tn = sum((y_true==0) & (y_pred==0))
+        
+        logs["val_tp"] = val_tp
+        logs["val_tn"] = val_tn
         
 rec_tptn = Record_tp_tn()
 
+model = build_mlp(input_shape=x_train.shape[1:])
+model.summary()
+optimizer = keras.optimizers.SGD(lr=LEARNING_RATE, nesterov=True, momentum=MOMENTUM)
+model.compile(loss="categorical_crossentropy", metrics=["accuracy"], optimizer=optimizer)
 
+model.fit(x_train, y_train, 
+          epochs=EPOCHS, 
+          batch_size=BATCH_SIZE, 
+          validation_data=(x_test, y_test), 
+          shuffle=True,
+          callbacks=[rec_tptn]
+         )
+
+# Collect results
+train_loss = model.history.history["loss"]
+valid_loss = model.history.history["val_loss"]
+train_acc = model.history.history["accuracy"]
+valid_acc = model.history.history["val_accuracy"]
+
+valid_tp = model.history.history['val_tp']
+valid_tn = model.history.history['val_tn']
+
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+plt.plot(range(len(train_loss)), train_loss, label="train loss")
+plt.plot(range(len(valid_loss)), valid_loss, label="valid loss")
+plt.legend()
+plt.title("Loss")
+plt.show()
+
+plt.plot(range(len(train_acc)), train_acc, label="train accuracy")
+plt.plot(range(len(valid_acc)), valid_acc, label="valid accuracy")
+plt.legend()
+plt.title("Accuracy")
+plt.show()
+
+plt.plot(range(len(valid_tp)), valid_tp, label="valid tp", color="navy")
+plt.plot(range(len(valid_tn)), valid_tn, label="valid tn", color="red")
+plt.legend()
+plt.title("True positives and True Negatives")
+plt.show()
 
