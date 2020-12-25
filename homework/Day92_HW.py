df304781335a40d26fe522f0a53728f5b8f4d564
huayang3 <66266051+huayang3@users.noreply.github.com>
2020-12-25 18:58:24 +0800

    Day92_HW

---
+++ homework/Day92_HW.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 25 18:29:27 2020
+
+@author: hua_yang
+"""
+
+#作業１：若使用總共 128 個濾波器 (filters)，對一張 256x256 的影像做完卷積後，共會得到幾張特徵圖 (feature maps)？
+#ans:
+#作業２：卷積神經網路中，濾波器的大小 (3x3 or 5x5) 與內部數字的值(value) 是怎麼決定的呢
+#ans:
