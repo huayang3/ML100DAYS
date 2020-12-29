7d740c2899ccdeb4115255cf8a7234e5c10b3aa7
huayang3 <66266051+huayang3@users.noreply.github.com>
2020-12-29 16:19:41 +0800

    Day96_HW

---
+++ homework/Day96_HW.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 29 15:13:39 2020
+
+@author: hua_yang
+"""
+
+'''
+1.是否有對資料做標準化 (normalization)? 如果有，在哪幾行?
+ANS: 35, 36
+    
+2.使用的優化器 Optimizer 為何?
+ANS:Adadelta
+    
+3.模型總共疊了幾層卷積層?
+ANS:2
+    
+4.模型的參數量是多少?
+ANS: 1199882
+    
+'''
