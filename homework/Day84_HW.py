fb69c3856f2882988c74bb6912386d823e998884
huayang3 <66266051+huayang3@users.noreply.github.com>
2020-12-30 20:33:23 +0800

    Day84_HW

--- homework/Day84_HW.py
+++ homework/Day84_HW.py
@@ -7,8 +7,7 @@ Created on Wed Dec 30 20:21:58 2020
 
 import os
 import keras
-from keras.layers import BatchNormalization, Dropout
-
+import itertools
 # Disable GPU
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
@@ -37,71 +36,100 @@ x_test = preproc_x(x_test)
 y_train = preproc_y(y_train)
 y_test = preproc_y(y_test)
 
-def build_mlp(input_shape, output_units=10, num_neurons=[512, 256, 128]):
+from keras.layers import BatchNormalization, Activation, Dropout, regularizers
+
+def build_mlp(input_shape, 
+              output_units=10, 
+              num_neurons=[512, 256, 128],
+              use_bn=True,
+              drp_ratio=0.,
+              l2_ratio=0.):
     input_layer = keras.layers.Input(input_shape)
     
     for i, n_units in enumerate(num_neurons):
         if i == 0:
-            x = keras.layers.Dense(units=n_units, activation="relu", name="hidden_layer"+str(i+1))(input_layer)
-            x = BatchNormalization()(x)
+            x = keras.layers.Dense(units=n_units, 
+                                   kernel_regularizer=regularizers.l2(l2_ratio),
+                                   name="hidden_layer"+str(i+1))(input_layer)
+
+            if use_bn:
+                x = BatchNormalization()(x)
+            x = Activation("relu")(x)
+            x = Dropout(drp_ratio)(x)
+
         else:
-            x = keras.layers.Dense(units=n_units, activation="relu", name="hidden_layer"+str(i+1))(x)
-            x = BatchNormalization()(x)
-    
-    x = Dropout(0.5)(x)
+            x = keras.layers.Dense(units=n_units, 
+                                   kernel_regularizer=regularizers.l2(l2_ratio),
+                                   name="hidden_layer"+str(i+1))(x)
+            if use_bn:
+                x = BatchNormalization()(x)
+            x = Activation("relu")(x)
+            x = Dropout(drp_ratio)(x)
+            
     out = keras.layers.Dense(units=output_units, activation="softmax", name="output")(x)
-    
     model = keras.models.Model(inputs=[input_layer], outputs=[out])
     return model
 
 ## 超參數設定
-LEARNING_RATE = 0.01
-EPOCHS = 20
-BATCH_SIZE = 256
-MOMENTUM = 0.95
-OPTIMIZER = [keras.optimizers.SGD, keras.optimizers.RMSprop, keras.optimizers.Adagrad, keras.optimizers.Adam]
+"""
+Set your hyper-parameters
+"""
+LEARNING_RATE = 1e-3
+EPOCHS = 3
+BATCH_SIZE = 128
 
+"""
+建立實驗組合
+"""
+USE_BN = [True, False]
+DRP_RATIO = [0., 0.4, 0.8]
+L2_RATIO = [0., 1e-6, 1e-8]
+
+import keras.backend as K
+
+"""
+以迴圈方式遍歷組合來訓練模型
+"""
 results = {}
-"""
-使用迴圈，建立不同 MOMENTUM 的模型並訓練
-"""
-for opti in OPTIMIZER:
-    keras.backend.clear_session() # 把舊的 Graph 清掉
-    print("Experiment with  Optimizer = %s" % str(opti))
-    model = build_mlp(input_shape=x_train.shape[1:])
+for i, (use_bn, drp_ratio, l2_ratio) in enumerate(itertools.product(USE_BN, DRP_RATIO, L2_RATIO)):
+    K.clear_session()
+    print("Numbers of exp: %i, with bn: %s, drp_ratio: %.2f, l2_ratio: %.2f" % (i, use_bn, drp_ratio, l2_ratio))
+    model = build_mlp(input_shape=x_train.shape[1:], use_bn=use_bn, drp_ratio=drp_ratio, l2_ratio=l2_ratio)
     model.summary()
-    optimizer = opti(lr=LEARNING_RATE)
-    #optimizer = keras.optimizers.SGD(lr=LEARNING_RATE, nesterov=True, momentum=mtum)
+    optimizer = keras.optimizers.Adam(lr=LEARNING_RATE)
     model.compile(loss="categorical_crossentropy", metrics=["accuracy"], optimizer=optimizer)
 
     model.fit(x_train, y_train, 
               epochs=EPOCHS, 
               batch_size=BATCH_SIZE, 
               validation_data=(x_test, y_test), 
+              verbose=1,
               shuffle=True)
     
     # Collect results
-    train_loss = model.history.history["loss"]
-    valid_loss = model.history.history["val_loss"]
-    train_acc = model.history.history["accuracy"]
-    valid_acc = model.history.history["val_accuracy"]
-    
-    exp_name_tag = "exp-opti-%s" % str(opti)
-    results[exp_name_tag] = {'train-loss': train_loss,
-                             'valid-loss': valid_loss,
-                             'train-acc': train_acc,
-                             'valid-acc': valid_acc}
-    
+    exp_name_tag = ("exp-%s" % (i))
+    results[exp_name_tag] = {'train-loss': model.history.history["loss"],
+                             'valid-loss': model.history.history["val_loss"],
+                             'train-acc': model.history.history["accuracy"],
+                             'valid-acc': model.history.history["val_accuracy"]}
+
 import matplotlib.pyplot as plt
+import matplotlib.cm as mplcm
+import matplotlib.colors as colors
 #%matplotlib inline
-color_bar = ["r", "g", "b", "y", "m", "k"]
+NUM_COLORS = 20
+
+cm = plt.get_cmap('gist_rainbow')
+cNorm  = colors.Normalize(vmin=0, vmax=NUM_COLORS-1)
+scalarMap = mplcm.ScalarMappable(norm=cNorm, cmap=cm)
+color_bar = [scalarMap.to_rgba(i) for i in range(NUM_COLORS)]
 
 plt.figure(figsize=(8,6))
 for i, cond in enumerate(results.keys()):
     plt.plot(range(len(results[cond]['train-loss'])),results[cond]['train-loss'], '-', label=cond, color=color_bar[i])
     plt.plot(range(len(results[cond]['valid-loss'])),results[cond]['valid-loss'], '--', label=cond, color=color_bar[i])
 plt.title("Loss")
-plt.legend()
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
 
 plt.figure(figsize=(8,6))
@@ -109,5 +137,5 @@ for i, cond in enumerate(results.keys()):
     plt.plot(range(len(results[cond]['train-acc'])),results[cond]['train-acc'], '-', label=cond, color=color_bar[i])
     plt.plot(range(len(results[cond]['valid-acc'])),results[cond]['valid-acc'], '--', label=cond, color=color_bar[i])
 plt.title("Accuracy")
-plt.legend()
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
